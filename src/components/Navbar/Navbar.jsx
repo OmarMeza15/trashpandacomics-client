8f2daec14127f57b7ce118024d65a9393d27bae4
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-08 20:37:11 +0100

    added styles to the navbar when no one is logged in

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,13 +9,47 @@ function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+    <nav className="bg-black text-white flex items-center justify-between navbar">
+
+      {/* Logo and Brand */}
+      <div className="flex items-center brand">
+        <Link to="/">
+          <img 
+            src="https://static.wixstatic.com/media/160c7a_e9cdc9d1fd634d90b3a9cead7c2cd9a0~mv2.png/v1/fill/w_60,h_60,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/logowhitebg.png"
+            alt=""
+          />
+        </Link>
+
+        <p className="font-bold text-lg ml-4">TRASHPANDACOMIC</p>
+      </div>
+
+      {/* Middle links */}
+      <div className="font-light text-sm middleNav">
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+        <Link to="/">
+          <button>Shop</button>
+        </Link>
+        <Link to="/">
+          <button>FAQ</button>
+        </Link>
+        <Link to="/">
+          <button>Contact</button>
+        </Link>
+        <Link to="/">
+          <button>Availability</button>
+        </Link>
+        <Link to="/">
+          <button>About</button>
+        </Link>
+        <Link to="/">
+          <button>Reviews</button>
+        </Link>
+      </div>
 
       {isLoggedIn && (
-        <>
+        <div>
           <button onClick={logOutUser}>Logout</button>
 
           <Link to="/profile">
@@ -24,20 +58,32 @@ function Navbar() {
           </Link>
 
           <span>{user && user.name}</span>
-        </>
+        </div>
       )}
 
       {!isLoggedIn && (
-        <>
+        <div className="logSignButtons grid grid-cols-2 gap-x-2">
+
+          {/* Signup and Login buttons */}
           <Link to="/signup">
             {" "}
-            <button>Sign Up</button>{" "}
+            <button
+              className="inline-block rounded-lg bg-white px-4 py-1.5 text-base font-semibold leading-7 text-black shadow-sm ring-1  ring-gray-400 hover:bg-black hover:ring-white hover:text-white"
+              >
+              Sign Up
+            </button>{" "}
           </Link>
+
           <Link to="/login">
             {" "}
-            <button>Login</button>{" "}
+            <button
+              className="inline-block rounded-lg bg-black px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1     ring-white hover:bg-white hover:ring-gray-400 hover:text-black"
+              >
+              Login
+            </button>{" "}
           </Link>
-        </>
+
+        </div>
       )}
     </nav>
   );
