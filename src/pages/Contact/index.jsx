3ea299170793d01537d32bf49c70a5dca3229782
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-10 21:47:36 +0100

    added the form and styles for the contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
+import SocialMediaLinks from '../../components/SocialMediaLinks'
 
 const Contact = () => {
   return (
-    <div className="h-screen bg-black text-white">
+    <div className="bg-black text-white">
 
-    <h1>Contact form</h1>
+      <div className="h-screen flex flex-col justify-center items-center">
+        {/* Title and subtitle */}
+        <div className="w-1/2">
+          <h1 className="text-4xl font-medium mb-10">GET IN TOUCH</h1>
+          <p className="font-light">Please take the time to fill out the following form before placing your custom order. We will get back to you as soon as possible to discuss further details and confirm your deadline. Thank you!</p>
+        </div>
+
+        <form className="w-2/3">
+          {/* Email input */}
+          <label className="block mb-2 mt-10">Email:</label>
+          <div className="relative mb-6">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+            </div>
+
+            <input type="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block        w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@email.com"/>
+          </div>
+
+          {/* Name input */}
+          <label className="block mb-2">Name:</label>
+          <div className="relative mb-6">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg className="w-6 h-6 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"></path></svg>
+            </div>
+
+            <input type="text" name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  block       w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Doe"/>
+          </div>
+
+          {/* Comment input */}
+          <label className="block mb-2">Comment:</label>
+          <div className="relative mb-6">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg class="w-6 h-6 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path></svg>
+            </div>
+
+            <input type="password" name="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  block       w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="..."/>
+          </div>
+
+          {/* Submit button */}
+          <button type="submit" className="bg-white text-black w-36 h-11 rounded-3xl font-light mt-5 hover:opacity-50 transition ease-in-out duration-500">
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <SocialMediaLinks />
 
     </div>
   )
