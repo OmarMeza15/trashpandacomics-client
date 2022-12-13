bdf42a0030dd023337e325fb0855a82e72244c4b
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-13 20:33:13 +0100

    replaced the component comment with a new one called makeReview and added the functionality of updating the reviews in real time

--- src/pages/Reviews/index.jsx
+++ src/pages/Reviews/index.jsx
@@ -1,18 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import CommentModal from '../../components/CommentModal'
+import React, { useEffect, useState, useContext } from 'react'
+import MakeReview from '../../components/MakeReview'
 import SocialMediaLinks from '../../components/SocialMediaLinks'
 import reviewService from '../../services/review.service'
+import { AuthContext } from '../../context/auth.context'
 
 const Reviews = () => {
   const [reviews, setReviews] = useState([])
-  
-  useEffect(() => {
+
+  const { user } = useContext(AuthContext)
+  console.log(user)
+
+  const getReviews = () => {
     reviewService
       .getAllReviews()
       .then((response) => {
         setReviews(response.data)
       })
       .catch(console.log)
+  }
+
+  useEffect(() => {
+    getReviews()
   }, [])
  
 
@@ -26,17 +34,17 @@ const Reviews = () => {
           <p className='font-light'>Take a look at what other people think of our work and leave your review too.</p>
 
           {/* Leave a review component */}
-          <CommentModal />
+          <MakeReview getReviews={getReviews}/>
     
         </div>  
 
         {/* Reviews */}
         <div className='border rounded-xl border-white text-left w-2/3'>
 
-          {reviews.map(({ _id, createdAt, text, imageUrl }) => (
+          {reviews.map(({ _id, createdAt, text, imageUrl, author }) => (
             <div className='p-10 flex justify-center items-center gap-16' key={ _id }>
               <div>
-                <h2 className='text-xl'>Casey Lorimer</h2>
+                <h2 className='text-xl'>{author?.name}</h2>
                 <h3 className='font-light mb-5'>{ createdAt }</h3>
                 <p>{ text }</p>
               </div>
