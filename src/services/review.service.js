8aaa1b57c9369ae4f3b9c251409e1e0c1b1ac13a
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-13 04:45:59 +0100

    created the reviews service

---
+++ src/services/review.service.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+class ReviewService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:5005"
+    });
+
+    // Automatically set JWT token in the headers for every request
+    this.api.interceptors.request.use((config) => {
+      // Retrieve the JWT token from the local storage
+      const storedToken = localStorage.getItem("authToken");
+
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+
+      return config;
+    });
+  }
+
+  // POST /api/examples
+  createReview = async (requestBody) => {
+    return this.api.post('/review/reviews', requestBody);
+  }
+
+  // GET /api/examples
+  getAllReviews = async () => {
+    return this.api.get('/review/reviews');
+  }
+
+  // GET /api/examples/:id
+  getOne = async (id) => {
+    return this.api.get(`/api/examples/${id}`);
+  }
+
+  // PUT /api/examples/:id
+  updateOne = async (id, requestBody) => {
+    return this.api.put(`/api/examples/${id}`, requestBody);
+  }
+
+  // DELETE /api/examples/:id
+  deleteProject = async (id) => {
+    return this.api.delete(`/api/examples/${id}`);
+  } 
+
+
+}
+
+// Create one instance of the service
+const reviewService = new ReviewService();
+
+export default reviewService;
