6744b8a73fd4029f51f6cf5ad79782ef234f2021
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-14 22:58:36 +0100

    added a new dropdown item if the user is admin and another item that shows if the user is admin or guest

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -64,6 +64,17 @@ function Navbar() {
               </Dropdown.Item>
             </Link>
 
+            {/* Create a new product */}
+            {user.roles === "Admin" && (
+              <Link to="/new-product">
+                <Dropdown.Item>
+                  <p>Add product</p>
+
+                  <svg className="w-6 h-6 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd"></path></svg>
+                </Dropdown.Item>
+              </Link>
+            )}            
+
             <Dropdown.Divider />
 
             {/* Logout */}
@@ -71,6 +82,10 @@ function Navbar() {
               <p>Logout</p>
 
               <svg className="w-4 h-4 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd"></path></svg>
+            </Dropdown.Item>
+
+            <Dropdown.Item>
+              <p>role: {user.roles}</p>
             </Dropdown.Item>
           </Dropdown>
 
