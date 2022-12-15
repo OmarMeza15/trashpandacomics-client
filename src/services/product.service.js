2031777b538f3b64f5d804ff34702a5a5b2e6bad
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-15 07:05:18 +0100

    created the services for the products

---
+++ src/services/product.service.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+class ProductService {
+  constructor() {
+    this.api = axios.create({
+      baseURL: process.env.REACT_APP_SERVER_URL || "http://localhost:5005"
+    });
+
+    // Automatically set JWT token in the headers for every request
+    this.api.interceptors.request.use((config) => {
+      // Retrieve the JWT token from the local storage
+      const storedToken = localStorage.getItem("authToken");
+
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+
+      return config;
+    });
+  }
+
+  // POST /reviews/review
+  createProduct = async (requestBody) => {
+    return this.api.post('/product/new-product', requestBody);
+  }
+
+  // GET /reviews/review
+  getAllProducts = async () => {
+    return this.api.get('/product/shop');
+  }
+
+  // GET /reviews/review/upload
+  uploadImage = (file) => {
+    return this.api.post('/product/new-product/upload', file)
+      .then(res => res.data)
+      .catch(console.log);
+  };
+
+}
+
+// Create one instance of the service
+const productService = new ProductService();
+
+export default productService;
