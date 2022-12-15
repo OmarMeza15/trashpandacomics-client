a84464d74ed03fdd98781ade265ce9ba75481981
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-15 23:20:27 +0100

    fixed the classes

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -4,22 +4,22 @@ import "./NotFoundPage.css";
 function NotFoundPage() {
   return (
     <div className="bg-black h-screen text-white">
-      <div class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16 bg-black">
-        <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
-            <div class="flex flex-col justify-center content-center ">
+      <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16 bg-black">
+        <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+            <div className="flex flex-col justify-center content-center ">
                 <div className="flex justify-center mb-10">
                     <img 
                       src="https://i.ibb.co/G9DC8S0/404-2.png" 
                       alt="404"
                     />
                 </div>
-                <div class="">
-                    <div class="">
-                        <h1 class="my-2 font-bold text-2xl">
+                <div className="">
+                    <div className="">
+                        <h1 className="my-2 font-bold text-2xl">
                             Looks like you've found the
                             doorway to the great nothing
                         </h1>
-                        <p class="my-2">Sorry about that! Please visit our homepage to get where you need to go.</p>
+                        <p className="my-2">Sorry about that! Please visit our homepage to get where you need to go.</p>
                         <Link to="/">
                           <button
                             className="bg-white border border-white text-black w-1/4 h-11 mt-5 rounded-3xl font-light hover:bg-black hover:text-white transition ease-in-out duration-500"
