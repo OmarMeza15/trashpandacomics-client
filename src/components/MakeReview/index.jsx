d8350695ebd380f13ab31530654ab7e440820c37
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-15 07:09:32 +0100

    changed some styles

--- src/components/MakeReview/index.jsx
+++ src/components/MakeReview/index.jsx
@@ -41,7 +41,7 @@ const MakeReview = ({getReviews}) => {
               
               {/* Comment input */}
 
-              <div className='flex justify-center items-center gap-x-36 mt-10'>
+              <div className='flex justify-center items-center gap-x-36 mt-10 pb-10'>
                 <div className="relative w-1/3 h-64">
                   <div className="absolute mt-2 left-0 flex items-center pl-3 pointer-events-none">
                     <svg className="w-6 h-6 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path></svg>
@@ -71,10 +71,6 @@ const MakeReview = ({getReviews}) => {
                     Comment
                   </button>
                 </div>
-              </div>
-
-              {/* Button */}
-              <div className="pb-10">
               </div>
 
             </form>
