71a45812151d573027fa0922b6ae4b35a4cc1d87
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-16 18:10:17 +0100

    changed the place holders

--- src/pages/NewProduct/index.jsx
+++ src/pages/NewProduct/index.jsx
@@ -78,7 +78,7 @@ const NewProduct = () => {
               name="info" 
               value={info}
               onChange={(e) => setInfo(e.target.value)}
-              placeholder="Comic Book"
+              placeholder="Info"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block        w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             />
           </div>
@@ -97,7 +97,7 @@ const NewProduct = () => {
               name="description" 
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              placeholder="Comic Book"
+              placeholder="Description"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block        w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             />
           </div>
