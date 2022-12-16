3a9797ba9a8bf335238a6e80b369f1a9baf7e470
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-16 17:23:54 +0100

    created the component slide over

---
+++ src/components/Slideover/index.jsx
@@ -0,0 +1,199 @@
+import { Fragment, useEffect, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import productService from '../../services/product.service'
+
+const products = [
+  {
+    id: 1,
+    name: 'Throwback Hip Bag',
+    href: '#',
+    color: 'Salmon',
+    price: '$90.00',
+    quantity: 1,
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+    imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+  },
+  {
+    id: 2,
+    name: 'Medium Stuff Satchel',
+    href: '#',
+    color: 'Blue',
+    price: '$32.00',
+    quantity: 1,
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+    imageAlt:
+      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+  },
+  // More products...
+]
+
+const SlideOver = () => {
+  const [open, setOpen] = useState(false)
+  const [cart, setCart] = useState([])
+  const [details, setDetails] = useState({})
+
+  const getCart = () => {
+    productService
+      .getAllCart()
+      .then((response) => {
+        setCart(response.data)
+      })
+      .catch(console.log)
+  }
+
+  const getAllDetails = () => {
+    productService
+      .getProductDetails(id)
+      .then((response) => {
+        setDetails(response.data)
+      })
+      .catch(console.log)
+  }
+
+  useEffect(() => {
+    getCart()
+    getProducts()
+  }, [])
+
+  return (
+    <div>
+        {/* Open the slide over */}
+        <button onClick={() => setOpen(true)}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 hover:opacity-50">
+                <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+            </svg>
+        </button>
+        
+        {/* Slide over */}
+        <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+            <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                    as={Fragment}
+                    enter="transform transition ease-in-out duration-500 sm:duration-700"
+                    enterFrom="translate-x-full"
+                    enterTo="translate-x-0"
+                    leave="transform transition ease-in-out duration-500 sm:duration-700"
+                    leaveFrom="translate-x-0"
+                    leaveTo="translate-x-full"
+                >
+                    {/* CLose the slide over */}
+                    <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                        <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
+                        <div className="flex items-start justify-between">
+                            <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
+                            <div className="ml-3 flex h-7 items-center">
+                            <button
+                                type="button"
+                                className="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                                onClick={() => setOpen(false)}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                  <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+                                </svg>
+                            </button>
+                            </div>
+                        </div>
+
+                        <div className="mt-8">
+                            <div className="flow-root">
+                            <ul role="list" className="-my-6 divide-y divide-gray-200">
+
+                                {cart.map(({product, quantity}) => (
+                                <li key={product._id} className="flex py-6">
+                                    <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                    <img
+                                        src={product.imageUrl}
+                                        alt={product.imageUrl}
+                                        className="h-full w-full object-cover object-center"
+                                    />
+                                    </div>
+
+                                    <div className="ml-4 flex flex-1 flex-col">
+                                    <div>
+                                        <div className="flex justify-between text-base font-medium text-gray-900">
+                                        <h3>
+                                            <p>{product.title}</p>
+                                        </h3>
+                                        <p className="ml-4">{product.price}</p>
+                                        </div>
+                                        <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                    </div>
+                                    <div className="flex flex-1 items-end justify-between text-sm">
+                                        <p className="text-gray-500">Qty {quantity}</p>
+
+                                        <div className="flex">
+                                        <button
+                                            type="button"
+                                            className="font-medium text-indigo-600 hover:text-indigo-500"
+                                        >
+                                            Remove
+                                        </button>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </li>
+                                ))}
+
+                            </ul>
+                            </div>
+                        </div>
+                        </div>
+
+                        <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
+                        <div className="flex justify-between text-base font-medium text-gray-900">
+                            <p>Subtotal</p>
+                            <p>$262.00</p>
+                        </div>
+                        <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                        <div className="mt-6">
+                            <a
+                            href="/"
+                            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                            alt=""
+                            >
+                            Checkout
+                            </a>
+                        </div>
+                        <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                            <p>
+                            or
+                            <button
+                                type="button"
+                                className="font-medium text-indigo-600 hover:text-indigo-500"
+                                onClick={() => setOpen(false)}
+                            >
+                                Continue Shopping
+                                <span aria-hidden="true"> &rarr;</span>
+                            </button>
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    </Dialog.Panel>
+                </Transition.Child>
+                </div>
+            </div>
+            </div>
+        </Dialog>
+        </Transition.Root>
+    </div>
+  )
+}
+
+export default SlideOver
